60ee3823ebc0572b3f57b4b3d55f7772ec5f43c5
IT012312 <it012312@MacBook-Air-2.local>
2023-03-02 11:55:04 +0530

    weather 2.0

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 async function weather() {
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=London&aqi=no");
 	// const data = await response.json();
-    return await response.json();
+    return response.json();
 	// console.log(data);
     // show();
     
@@ -14,8 +14,14 @@ let result=weather()
     document.getElementById("temparature").innerHTML= res.current.temp_f + "\u00B0";
     document.getElementById("feel-temparature").innerHTML= "Feels " +res.current.feelslike_f + "\u00B0";
 
-    // if(res.current.text === "Clear")
+    if(res.current.condition.text === "Partly cloudy")
+    document.getElementById("weather-image").innerHTML = "<i class='fa fa-cloud-sun fa-5x mt-4' style='color:orange'></i>"
+
+    if(res.current.condition.text === "Clear")
+    document.getElementById("weather-image").innerHTML = "<i class='fa fa-sun fa-5x mt-4' style='color:orange'></i>"
     
+    if(res.current.condition.text === "Cloudy")
+    document.getElementById("weather-image").innerHTML = "<i class='fa fa-cloud fa-5x mt-4' style='color:grey'></i>"
 })
 
 
