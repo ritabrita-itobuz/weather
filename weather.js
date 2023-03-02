05e9eae9aeb6c2c8b8e5658c51082969f1de5224
IT012312 <it012312@MacBook-Air-2.local>
2023-03-02 18:58:39 +0530

    weather 3.0

--- weather.js
+++ weather.js
@@ -1,28 +1,44 @@
-async function weather() {
-    const response = await fetch("http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=London&aqi=no");
-	// const data = await response.json();
-    return response.json();
-	// console.log(data);
-    // show();
+
+var api = "";
+
+var data = async function () {
+    const response = await fetch(api);
+    return await response.json();    
+}
+let icon = (image) => {
+    let weathericon = document.getElementById("weather-image");
+    while (weathericon.firstChild){
+        weathericon.removeChild(weathericon.firstChild);
+    }
+    let element = document.createElement("img");
+    element.setAttribute("src", image);
+    element.setAttribute("alt","Weather");
+    document.getElementById("weather-image").appendChild(element);
+
+}
+function getLocation() {
+    let val = document.querySelector('input').value;
+    api = "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" + val + "&aqi=no";
+    console.log(api);
+    let content=data()
+    .then((response)=>{
+        console.log(response);
+        document.getElementById("actual-temparature").innerHTML = response.current.temp_c + "\u00B0";
+        document.getElementById("feel-like-temparature").innerHTML = "Feels  " + response.current.feelslike_c + "\u00B0";
+        // icon('images/partly-cloudy.png');
+        if(response.current.condition.text == "Mist")
+            {
+                icon('images/mist.png');
+            }
+        if(response.current.condition.text == "Partly cloudy")
+            {
+                icon('images/partly-cloudy.png');
+            }
     
+
+})
 }
 
-let result=weather()
-.then((res)=>{
-    console.log(res);
-    document.getElementById("location").innerHTML= res.location.name;
-    document.getElementById("temparature").innerHTML= res.current.temp_f + "\u00B0";
-    document.getElementById("feel-temparature").innerHTML= "Feels " +res.current.feelslike_f + "\u00B0";
-
-    if(res.current.condition.text === "Partly cloudy")
-    document.getElementById("weather-image").innerHTML = "<i class='fa fa-cloud-sun fa-5x mt-4' style='color:orange'></i>"
-
-    if(res.current.condition.text === "Clear")
-    document.getElementById("weather-image").innerHTML = "<i class='fa fa-sun fa-5x mt-4' style='color:orange'></i>"
-    
-    if(res.current.condition.text === "Cloudy")
-    document.getElementById("weather-image").innerHTML = "<i class='fa fa-cloud fa-5x mt-4' style='color:grey'></i>"
-})
 
 
 
