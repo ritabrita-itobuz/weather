66b08202e36e07691f26443caca753ae58a882cf
IT012312 <it012312@MacBook-Air-2.local>
2023-03-02 10:58:29 +0530

    working in progress

--- weather.js
+++ weather.js
@@ -1,13 +1,22 @@
 async function weather() {
     const response = await fetch("http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=London&aqi=no");
-	const data = await response.json();
-	console.log(data);
+	// const data = await response.json();
+    return await response.json();
+	// console.log(data);
     // show();
+    
 }
 
-weather();
-document.getElementById("location").innerHTML= data.location.name;
-// function show() {
-//     let location = document.querySelector("#location");
-//     location.innerHTML = data;
-// }
+let result=weather()
+.then((res)=>{
+    console.log(res);
+    document.getElementById("location").innerHTML= res.location.name;
+    document.getElementById("temparature").innerHTML= res.current.temp_f + "\u00B0";
+    document.getElementById("feel-temparature").innerHTML= "Feels " +res.current.feelslike_f + "\u00B0";
+
+    // if(res.current.text === "Clear")
+    
+})
+
+
+
